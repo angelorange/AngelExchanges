c7fcc1b142bfe946dead7dc0ec08f68790eba4e4
angelorange <acostalarangeira@gmail.com>
2019-09-18 15:36:36 -0300

    add tether and change bootstrap import

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require bootstrap/dist/js/bootstrap
+//= require bootstrap/dist/js/bootstrap.bundle
 //= require jquery
+//= require tether/dist/js/tether
 //= require_tree .
